df804f9cb4e0064fcc3e9e99578c419fc043796e
Rahul Singh <rahulsingh@Rahuls-MacBook-Air.local>
2023-07-20 12:24:46 +0530

    ep-1

---
+++ App.js
@@ -0,0 +1,20 @@
+
+ /*const heading = React.createElement("h1", 
+ {id: "heading", xyz = "abc"},
+ "Hello world1");
+
+ console.log(heading);
+ */
+
+
+const parent = React.createElement("div",
+  {id = "parent"},
+   React.createElement("div", {id = "child"}, 
+   [React.createElement("h1", {}, "I am h1 tag"),
+   React.createElement("h2", {}, "I am h2 tag")]
+   ) )
+
+const root = ReactDom.createRoot(document.getElementById("root"));
+
+ root.render(parent);
+ 
